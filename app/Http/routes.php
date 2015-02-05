3150a3aeab46a88e6c7f456cb24790f247cef053
Jung Oh <Jung3o@yahoo.com>
2015-02-04 19:08:45 -0600

    finished servers

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,7 +28,82 @@ Route::get('/', array('as' => 'home', function()
 
 Route::get('/servers', array('as' => 'servers', function()
 {
-    return view('hihihihihihihihi/pages/servers');
+    $servers = array();
+
+    $servers[] = array(
+        'title' => 'Beginner',
+        'alt'   => 'beginner surf',
+        'img'   => 'http://cache.www.gametracker.com/server_info/74.91.120.159:27015/b_350_20_DB0000_520004_FFFFFF_000000.png',
+        'desc'  => 'This server is for players who are learning how to surf.',
+        'conn'  => 'steam://connect/74.91.120.159:27015'
+    );
+
+    $servers[] = array(
+        'title' => 'Easy',
+        'alt'   => 'easy surf',
+        'img'   => 'http://cache.www.gametracker.com/server_info/74.91.120.159:27017/b_350_20_DB0000_520004_FFFFFF_000000.png',
+        'desc'  => 'This server is for players who are decent at surfing.',
+        'conn'  => 'steam://connect/74.91.120.159:27017'
+    );
+
+    $servers[] = array(
+        'title' => 'Medium',
+        'alt'   => 'medium surf',
+        'img'   => 'http://cache.www.gametracker.com/server_info/74.91.120.159:27018/b_350_20_DB0000_520004_FFFFFF_000000.png',
+        'desc'  => 'This server is for players who are good at surfing.',
+        'conn'  => 'steam://connect/74.91.120.159:27018'
+    );
+
+    $servers[] = array(
+        'title' => 'Hard',
+        'alt'   => 'hard surf',
+        'img'   => 'http://cache.www.gametracker.com/server_info/74.91.120.159:27016/b_350_20_DB0000_520004_FFFFFF_000000.png',
+        'desc'  => 'This server is for skilled surfers. This is our most difficult server.',
+        'conn'  => 'steam://connect/74.91.120.159:27016'
+    );
+
+    $servers[] = array(
+        'title' => 'Donator',
+        'alt'   => 'donator surf',
+        'img'   => 'http://cache.www.gametracker.com/server_info/74.91.120.159:27019/b_350_20_DB0000_520004_FFFFFF_000000.png',
+        'desc'  => 'This server is for players who are decent at surfing.',
+        'conn'  => 'steam://connect/74.91.120.159:27019'
+    );
+
+    $servers[] = array(
+        'title' => 'Teamspeak 3',
+        'alt'   => 'teamspeak server',
+        'img'   => 'http://cache.www.gametracker.com/server_info/198.206.15.182:1854/b_350_20_DB0000_520004_FFFFFF_000000.png',
+        'desc'  => 'This is our Teamspeak Server. We use TS to communicate with players.',
+        'conn'  => 'ts3server://ts3.horizonservers.net:1854'
+    );
+
+    $ranks = array();
+
+    $ranks[] = array('rank' => 'Unranked | 0%','color' => 'grey');
+    $ranks[] = array('rank' => 'Newbie | 100%','color' => 'lightblue');
+    $ranks[] = array('rank' => 'Newbie ✪ | 75%','color' => 'lightblue');
+    $ranks[] = array('rank' => 'Learning | 50%','color' => 'default');
+    $ranks[] = array('rank' => 'Learning ✪ | 45%','color' => 'default');
+    $ranks[] = array('rank' => 'Novice | 40%','color' => 'yellow');
+    $ranks[] = array('rank' => 'Novice ✪ | 35%','color' => 'yellow');
+    $ranks[] = array('rank' => 'Average | 30%','color' => 'orange');
+    $ranks[] = array('rank' => 'Average ✪ | 25%','color' => 'orange');
+    $ranks[] = array('rank' => 'Advanced | 20%','color' => 'olive');
+    $ranks[] = array('rank' => 'Advanced ✪ | 10%','color' => 'olive');
+    $ranks[] = array('rank' => 'Skilled | 5%','color' => 'lime');
+    $ranks[] = array('rank' => 'Skilled ✪ | 2.5%','color' => 'lime');
+    $ranks[] = array('rank' => 'Expert | 1%','color' => 'purple');
+    $ranks[] = array('rank' => 'Expert ✪ | 0.87%','color' => 'purple');
+    $ranks[] = array('rank' => 'Master | 0.75%','color' => '#1E90FF');
+    $ranks[] = array('rank' => 'Master ✪ | 0.5%','color' => '#1E90FF');
+    $ranks[] = array('rank' => 'Legendary | 0.25%','color' => 'darkblue');
+    $ranks[] = array('rank' => 'Legendary ✪ | 0.15%','color' => 'darkblue');
+    $ranks[] = array('rank' => 'God-Like | 0.1%','color' => 'red');
+    $ranks[] = array('rank' => '✪ God-Like ✪ | 0.001%','color' => 'red');
+
+
+    return View::make('hihihihihihihihi/pages/servers', array('servers' => $servers, 'ranks' => $ranks));
 }));
 
 
--- resources/views/hihihihihihihihi/pages/servers.blade.php
+++ resources/views/hihihihihihihihi/pages/servers.blade.php
@@ -2,6 +2,38 @@
 
 @section('content')
 
-
+<div class="row">
+    <div class="col-xs-12 col-sm-push-4 col-sm-8 col-md-push-3 col-md-9">
+        <h3 class="title">Server List</h3>
+        <div class="servers">
+            @foreach($servers as $server)
+            <div class="item">
+                <div class="row">
+                    <div class="col-xs-12 col-md-8">
+                        <div class="header">
+                            <span class="small-title">Surf {!! $server['title'] !!}</span>
+                        </div>
+                        <div class="desc">
+                            <p>{!! $server['desc'] !!}</p>
+                        </div>
+                        <img class="img-responsive" src="{!! $server['img'] !!}" alt="{!! $server['alt'] !!}">
+                    </div>
+                    <div class="col-xs-12 col-md-4">
+                        <a href="{!! $server['conn'] !!}" class="btn btn-custom">Join {!! $server['title'] !!}</a>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-pull-8 col-sm-4 col-md-pull-9 col-md-3">
+        <h3 class="title">Ranks</h3>
+        <ul class="ranks">
+            @foreach($ranks as $rank)
+            <li style="color:{!! $rank['color'] !!}">{!! $rank['rank'] !!}</li>
+            @endforeach
+        </ul>
+    </div>
+</div>
 
 @stop
